dba7f45ffd2a67741a1404e2e2d4e0e0b308be17
Desktop-Wangq1h <wqh13812176226@outlook.com>
2024-12-14 21:50:13 +0800

    大更新

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// assets/js/main.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  var announcementModal = new bootstrap.Modal(document.getElementById('announcement-modal'));
+  announcementModal.show();
+});
