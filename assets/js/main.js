ae41456888f8c33d7c74ae3d6fb7fffb713daec4
Desktop-Wangq1h <wqh13812176226@outlook.com>
2024-12-14 22:28:00 +0800

    公告

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
 // assets/js/main.js
 
 document.addEventListener("DOMContentLoaded", function() {
-  if (!sessionStorage.getItem('announcementShown')) {
+  const announcementKey = 'announcementShown';
+  const announcementExpiryKey = 'announcementExpiry';
+  const now = new Date().getTime();
+  const oneDay = 24 * 60 * 60 * 1000; // 一天的毫秒数
+
+  const lastShown = localStorage.getItem(announcementKey);
+  const expiryTime = localStorage.getItem(announcementExpiryKey);
+
+  if (!lastShown || now > expiryTime) {
     var announcementModal = new bootstrap.Modal(document.getElementById('announcement-modal'));
     announcementModal.show();
-    sessionStorage.setItem('announcementShown', 'true');
+    localStorage.setItem(announcementKey, 'true');
+    localStorage.setItem(announcementExpiryKey, now + oneDay);
   }
 });
